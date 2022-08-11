0f3289ea0be110eba22e1a204ca054917478eb8d
Lukas Daniel <60492975+LukasDniel@users.noreply.github.com>
2022-08-11 00:45:40 -0300

    Add files via upload

---
+++ Landing Page - Starbucks/assets/js/main.js
@@ -0,0 +1,10 @@
+// CHANGE IMAGE DRINK
+function imgSlide(anything){
+    document.querySelector('.starbucks').src = anything;
+}
+
+// CHANGE BACKGROUND COLOR DRINK
+function changeCircleColor(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
